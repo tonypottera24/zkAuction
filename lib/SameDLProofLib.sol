425add075e28a01a3f86796fd047987da5cdddb6
Po-Chu Hsu <tonypottera@gmail.com>
2022-09-26 07:05:06 +0000

    refactor

--- lib/SameDLProofLib.sol
+++ lib/SameDLProofLib.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.8.0;
-pragma experimental ABIEncoderV2;
+pragma solidity >=0.8.0 <0.9.0;
 
 import {UIntLib} from "./UIntLib.sol";
 import {ECPoint, ECPointLib} from "./ECPointLib.sol";
@@ -22,18 +21,17 @@ library SameDLProofLib {
         ECPoint memory g2,
         ECPoint memory y1,
         ECPoint memory y2
-    ) internal pure returns (bool) {
-        bytes32 digest =
-            keccak256(
-                abi.encodePacked(
-                    g1.pack(),
-                    g2.pack(),
-                    y1.pack(),
-                    y2.pack(),
-                    pi.grr1.pack(),
-                    pi.grr2.pack()
-                )
-            );
+    ) internal returns (bool) {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                g1.pack(),
+                g2.pack(),
+                y1.pack(),
+                y2.pack(),
+                pi.grr1.pack(),
+                pi.grr2.pack()
+            )
+        );
         uint256 c = uint256(digest).modQ();
         return
             g1.scalar(pi.rrr).equals(pi.grr1.add(y1.scalar(c))) &&
@@ -46,7 +44,7 @@ library SameDLProofLib {
         ECPoint[] memory g2,
         ECPoint[] memory y1,
         ECPoint[] memory y2
-    ) internal pure returns (bool) {
+    ) internal returns (bool) {
         for (uint256 i = 0; i < pi.length; i++) {
             if (valid(pi[i], g1[i], g2[i], y1[i], y2[i]) == false) return false;
         }
